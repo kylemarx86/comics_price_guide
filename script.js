6aa3821221a71752fd0a4e11887ffd245d85aba7
kylemarx86 <kylemarx86@csu.fullerton.edu>
2017-09-01 16:32:35 -0700

    add method to retrieve secret identity based on character name.

--- script.js
+++ script.js
@@ -66,3 +66,42 @@ function retrieveDebut()
     }
 }
 
+function retrieveIdentity()
+{
+    // var searchValue = "the thing";
+    var searchValue = "thing";
+    // var searchValue = "Iron Man";
+    // var searchValue = "spider-man";
+    // var searchValue = "Captain Marvel";
+    
+    var script=document.createElement('script');
+    script.type='text/javascript';
+    script.src = 'https://marvel.wikia.com/api.php?format=json&action=query' +
+                 '&prop=revisions&titles=' +
+                 encodeURIComponent(searchValue) +
+                 '&rvprop=content&rvsection=0&callback=parseWiki';
+    scriptDiv = document.getElementById('wikicontent');
+    scriptDiv.innerHTML = '';
+    scriptDiv.appendChild(script)
+    window.parseWiki = function(result)
+    {
+        // console.log('result: ', result);
+        
+        var key = 0;
+        for(i in result.query.pages)
+        key = i;
+        
+        content = result.query.pages[key].revisions[0]['*'];
+        // console.log("content: ", content);
+
+
+        var identity = content.match(/Main Character\s*=\s(.*)\|/g)[0];
+        var delimiter = '= [[';
+        var startIndex = identity.indexOf(delimiter);
+        identity = identity.substring(startIndex + delimiter.length, identity.length - 1);
+
+        console.log(identity);
+
+    }
+}
+
