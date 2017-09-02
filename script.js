06a4977da39d14a2888cb20d4e2cafd780f65b28
kylemarx86 <kylemarx86@csu.fullerton.edu>
2017-09-02 14:18:02 -0700

    convert retrieveIdentity to use an ajax call.

--- script.js
+++ script.js
@@ -13,29 +13,32 @@ $(document).ready(function(){
   * @param {string} mantle - mantle (public name) of character to be looked up (for instance Iron Man or Spider-Man)
   */
 function retrieveIdentity(mantle){
-    var script=document.createElement('script');
-    script.type='text/javascript';
-    script.src = 'https://marvel.wikia.com/api.php?format=json&action=query&prop=revisions&rvprop=content&rvsection=0&callback=parseWiki'
-                 + '&titles=' + encodeURIComponent(mantle);
+    $.ajax({
+        type: "GET",
+        url: 'https://marvel.wikia.com/api.php?format=json&action=query&prop=revisions&rvprop=content&rvsection=0&callback=?&titles=' + encodeURIComponent(mantle),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data, textStatus, jqXHR) {
+            parseWiki(data);
+        },
+        error: function (errorMessage) {
+        }
+    });
+}
 
-                 
-    scriptDiv = document.getElementById('identity');
-    scriptDiv.innerHTML = '';
-    scriptDiv.appendChild(script)
-    window.parseWiki = function(result) {
-        var key = 0;
-        for(i in result.query.pages)
-        key = i;
-        
-        content = result.query.pages[key].revisions[0]['*'];
+function parseWiki(result){
+    var key = 0;
+    for(i in result.query.pages)
+    key = i;
+    
+    content = result.query.pages[key].revisions[0]['*'];
 
-        var identity = content.match(/Main Character\s*=\s(.*)\|/g)[0];
-        var delimiter = '= [[';
-        var startIndex = identity.indexOf(delimiter);
-        identity = identity.substring(startIndex + delimiter.length, identity.length - 1);
+    var identity = content.match(/Main Character\s*=\s(.*)\|/g)[0];
+    var delimiter = '= [[';
+    var startIndex = identity.indexOf(delimiter);
+    identity = identity.substring(startIndex + delimiter.length, identity.length - 1);
 
-        console.log(identity);
-    }
+    console.log(identity);
 }
 
  /**
