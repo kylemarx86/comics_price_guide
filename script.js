0a9b4018ea2a21030e508970c9f3e98755b710e0
kylemarx86 <kylemarx86@csu.fullerton.edu>
2017-09-02 01:32:15 -0700

    clean up and reorganize script. add comments.

--- script.js
+++ script.js
@@ -1,99 +1,33 @@
+$(document).ready(function(){
+    retrieveIdentity("Thing");
+    retrieveDebut("Benjamin Grimm (Earth-616)");
+});
+
 /**
  * Adapted from work by ujjawal found at https://github.com/ujjawal/Parse-Wiki-Infobox
  */
-function retrieveDebut()
-{
-    // var searchValue = "the thing";
-    // var searchValue = "Iron Man";
-    var searchValue = "Anthony Stark";
-    // var searchValue = "spider-man";
-    // var searchValue = "Captain Marvel";
-    
-    // var searchQualifier = " (comics)";
-    // var searchQualifier = " (Marvel Comics)";
-    // var searchQualifier = "";
-    var searchQualifier = " (Earth-616)";
 
+ /**
+  * retrieveIdentity
+  * searches Marvel wiki for mantle and will retrieve information on the character from which their real identity will be extracted
+  * @param {string} mantle - mantle (public name) of character to be looked up (for instance Iron Man or Spider-Man)
+  */
+function retrieveIdentity(mantle){
     var script=document.createElement('script');
     script.type='text/javascript';
-    script.src = 'https://marvel.wikia.com/api.php?format=json&action=query' +
-    // script.src = 'https://marvel.wikia.com/api.php?format=json&action=parse' +
-    // script.src = 'http://en.wikipedia.org/w/api.php?format=json&action=query' +
-                 '&prop=revisions&titles=' +
-                 encodeURIComponent(searchValue + searchQualifier) +
-                 '&rvprop=content&rvsection=0&callback=parseWiki';
-                //  '&rvprop=content&rvsection=0';
-    scriptDiv = document.getElementById('wikicontent');
+    script.src = 'https://marvel.wikia.com/api.php?format=json&action=query&prop=revisions&rvprop=content&rvsection=0&callback=parseWiki'
+                 + '&titles=' + encodeURIComponent(mantle);
+
+                 
+    scriptDiv = document.getElementById('identity');
     scriptDiv.innerHTML = '';
     scriptDiv.appendChild(script)
-    window.parseWiki = function(result)
-    {
-        // console.log('result: ', result);
-        
-        var key = 0;
-        for(i in result.query.pages)
-        key = i;
-        
-        // console.log('sub-result: ',result.query.pages[key].revisions["0"]["*"]);
-        content = result.query.pages[key].revisions[0]['*'];
-        // v = content.match(/{{[^]*?({{[^{}]*?}}[^]*?)*}}/g);
-        // v = content.match(/debut = ''\[\[(.*)\]\]'' #(\d*) \((\w*) (\d*)\)/g);
-
-
-        var debut = content.match(/\| First\s*=\s(.*)/g)[0];
-        var delimiter = '= ';
-        var startIndex = debut.indexOf(delimiter);
-        debut = debut.substring(startIndex + delimiter.length);
-
-        // v = content.match(/debut = ''\[\[(.*)\]\]'' #(\d*) \((\w*) (\d*)\)/g);
-
-
-        // for(i=0; i< v.length; i++) { 
-        //     if(v[i].match(/infobox/i)) { 
-        //         foo = v[i].match(/debut = ''\[\[(.*)\]\]'' #(\d*) \((\w*) (\d*)\)/g);
-        //         break;
-        //     }
-        // }
-        console.log(debut);
-        // arr = [];
-        // for(i=0;i<foo.length;i++) { 
-        //     arr.push(foo[i].match(/[A-Za-z].*[^\]]/)[0]);
-        //     anchor = document.createElement('p');
-        //     anchor.innerHTML = arr[i];
-        //     anchor.href = '#';
-        //     scriptDiv.appendChild(anchor);
-        // }
-    }
-}
-
-function retrieveIdentity()
-{
-    // var searchValue = "the thing";
-    var searchValue = "thing";
-    // var searchValue = "Iron Man";
-    // var searchValue = "spider-man";
-    // var searchValue = "Captain Marvel";
-    
-    var script=document.createElement('script');
-    script.type='text/javascript';
-    script.src = 'https://marvel.wikia.com/api.php?format=json&action=query' +
-                 '&prop=revisions&titles=' +
-                 encodeURIComponent(searchValue) +
-                 '&rvprop=content&rvsection=0&callback=parseWiki';
-    scriptDiv = document.getElementById('wikicontent');
-    scriptDiv.innerHTML = '';
-    scriptDiv.appendChild(script)
-    window.parseWiki = function(result)
-    {
-        // console.log('result: ', result);
-        
+    window.parseWiki = function(result) {
         var key = 0;
         for(i in result.query.pages)
         key = i;
         
         content = result.query.pages[key].revisions[0]['*'];
-        // console.log("content: ", content);
-
 
         var identity = content.match(/Main Character\s*=\s(.*)\|/g)[0];
         var delimiter = '= [[';
@@ -101,7 +35,37 @@ function retrieveIdentity()
         identity = identity.substring(startIndex + delimiter.length, identity.length - 1);
 
         console.log(identity);
+    }
+}
 
+ /**
+  * retrieveDebut
+  * searches the Marvel wiki for a character name (based on their true identity) and will retrieve information on the character from which their first appearance/debut comic will be extracted.
+  * @param {string} secretIdentity - real name of character looked up
+  */
+function retrieveDebut(secretIdentity){
+    var script=document.createElement('script');
+    script.type='text/javascript';
+    script.src = 'https://marvel.wikia.com/api.php?format=json&action=query&prop=revisions&rvprop=content&rvsection=0&callback=parseWiki2'
+                + '&titles=' + encodeURIComponent(secretIdentity);
+                 
+    scriptDiv = document.getElementById('debut');
+    scriptDiv.innerHTML = '';
+    scriptDiv.appendChild(script)
+    window.parseWiki2 = function(result)
+    {
+        var key = 0;
+        for(i in result.query.pages)
+        key = i;
+        
+        content = result.query.pages[key].revisions[0]['*'];
+
+        var debut = content.match(/\| First\s*=\s(.*)/g)[0];
+        var delimiter = '= ';
+        var startIndex = debut.indexOf(delimiter);
+        debut = debut.substring(startIndex + delimiter.length);
+
+        console.log(debut);
     }
 }
 
