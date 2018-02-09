005bf509c49d23bd589462a235184d87f103cb82
Kyle Marx <kylemarx86@csu.fullerton.edu>
2018-02-09 13:47:36 -0800

    initial node rework.

---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const port = process.env.PORT || 3000;
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+// app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public/script'));
+
+app.get('/', (req, res) => {
+    res.render('home.hbs');
+});
+
+app.listen(port, () => {
+    console.log(`server is up on port ${port}.`);
+});
